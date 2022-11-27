fc9ae5e1fdb2390bee9fa7db1aba2afac6c9e7a8
madalina.toma <madalina.toma@softem.ro>
2022-11-27 21:49:57 +0200

    Add project files.

---
+++ lab1ex10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab1ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Scrieti un program care sa va afisa suma cifrelor 
+            ununi numar intreg citit de la tastatura.
+            */
+
+            int number;
+          
+            Console.WriteLine("Introduceti numarul: ");
+            number = int.Parse(Console.ReadLine());
+           
+            int sum=0;
+            while(number>0)
+            {
+                sum = sum + (number % 10);
+                number = number / 10;
+            }
+            Console.WriteLine("Suma este: " + sum);
+        }
+    }
+}
